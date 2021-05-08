a7de4cec851932746993eb96f97e3041eb06f670
КВАНТОРИАНЕЦ <1>
2021-05-08 15:56:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+public class MainActivity extends Activity implements OnClickListener {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        Button startButton = (Button)findViewById(R.id.button1);
+        startButton.setOnClickListener(this);
+
+        Button exitButton = (Button)findViewById(R.id.button2);
+        exitButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1: {
+                Intent intent = new Intent();
+//                intent.setClass(this, Game.class);
+                startActivity(intent);
+            }break;
+
+            case R.id.button2: {
+                finish();
+            }break;
+
+            default:
+                break;
+        }
+    }
+}
